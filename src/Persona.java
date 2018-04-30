2363b270372feff6d15e573d592226783bec1dd1
Juan Londono <Juan Londono@JuanLondono>
2018-04-30 18:59:01 -0500

    se inserto informacion a la base de datos

--- src/Persona.java
+++ src/Persona.java
@@ -1,18 +1,29 @@
 import java.sql.*;
 import conexion.Conexion;
+import java.util.Calendar;
 import javax.swing.JOptionPane;
 import org.omg.CORBA.ORB;
+
 
 public class Persona extends PersonaApp.PersonaPOA{
     
     private ORB orb;
-
+    Conexion conex = new Conexion();
     @Override
     public boolean insertarPersona(int id_persona, String nombre, String apellido, int telefono, String direccion, String correo) {
         boolean resultado = false;
         try {
             String query = "Insert into persona(id_persona,nombre,apellido,telefono,direccion,correo)"
-                    + "values ("+id_persona+","+nombre+","+apellido+","+telefono+","+direccion+","+correo+")";
+                    + "values ('"+id_persona+"','"+nombre+"','"+apellido+"','"+telefono+"','"+direccion+"','"+correo+"')";
+            conex.conexion();
+            Statement st = conex.conex.createStatement();
+            int valor = st.executeUpdate(query);
+            if(valor>0){
+                return resultado= true;
+            }
+            //cerramos los recursos
+            st.close();
+            conex.conex.close();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Ocurrio un error " + e.getMessage());
         }
