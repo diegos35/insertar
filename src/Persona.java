21751e87cd2f9fd8f2f24190bf5c8921404fb69f
Juan Londono <Juan Londono@JuanLondono>
2018-05-02 21:24:07 -0500

    se crearon los botones actualizar y eliminar

--- src/Persona.java
+++ src/Persona.java
@@ -29,28 +29,106 @@ public class Persona extends PersonaApp.PersonaPOA{
         }
         return resultado;
     }
+    
 
     @Override
     public String consultarPersona(int id_persona) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       String resultado ="";
+         try {
+             String query = "select * from persona where id_persona ="+id_persona;
+             conex.conexion();
+                     Statement st = conex.conex.createStatement();
+                     ResultSet rs = st.executeQuery(query);
+                     while(rs.next()){
+                         resultado +=rs.getString(2)+ "  - "
+                                  +rs.getString(3)+ "  - "
+                                 +rs.getInt (4)+ "  - "
+                                 +rs.getString(5)+ "  - "
+                                 +rs.getString (6);
+                                   
+                     }
+                     st.close();
+                     rs.close();
+            conex.conex.close();
+            
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null,"ocurrio un error :"+e.getMessage());
+        }
+         return resultado;
     }
+    
 
     @Override
     public boolean eliminarPersona(int id_persona) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         boolean resultado =false;
+          try {
+            String query = "delete from persona where id_persona ="+id_persona;
+            conex.conexion();
+            Statement st = conex.conex.createStatement();
+            int valor = st.executeUpdate(query);
+            if(valor>0){
+                return resultado= true;
+            }
+            //cerramos los recursos
+            st.close();
+            conex.conex.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ocurrio un error " + e.getMessage());
+        }
+        return resultado;
     }
+
 
     @Override
     public boolean actualizarPersona(int id_persona, String nombre, String apellido, int telefono, String direccion, String correo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean resultado = false;
+        try {
+            String query = "UPDATE persona SET nombre='"+nombre+"',apellido='"+apellido+"',telefono='"+telefono+"',direccion='"+telefono+"',correo='"+correo+"' WHERE id_persona = "+id_persona;
+            conex.conexion();
+            Statement st = conex.conex.createStatement();
+            int valor = st.executeUpdate(query);
+            if(valor>0){
+                return resultado= true;
+            }
+            //cerramos los recursos
+            st.close();
+            conex.conex.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ocurrio un error " + e.getMessage());
+        }
+        return resultado;
+    
     }
 
     @Override
     public String listarPersonas() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         String resultado ="";
+         try {
+             String query = "select * from persona";
+             conex.conexion();
+                     Statement st = conex.conex.createStatement();
+                     ResultSet rs = st.executeQuery(query);
+                     while(rs.next()){
+                         resultado +=rs.getString(2)+ "  - "
+                                  +rs.getString(3)+ "  - "
+                                 +rs.getInt (4)+ "  - "
+                                 +rs.getString(5)+ "  - "
+                                 +rs.getString (6)+"\n"; 
+                                   
+                     }
+                     st.close();
+                     rs.close();
+            conex.conex.close();
+            
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null,"ocurrio un error :"+e.getMessage());
+        }
+         return resultado;
     }
 
     @Override
+    
+    
     public void shutdown() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
